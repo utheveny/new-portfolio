fcbb22b1f86fa10f40bb4f3dec3fc15a3ce5aa2a
S-i-d-V <me@ugotheveny.dev>
2024-01-03 01:32:15 +0100

    📦️ Install + Setup react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.scss'
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
